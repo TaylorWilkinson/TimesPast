e4c404ae389285fdc2da7fa1148eea0707e0c58a
Taylor Wilkinson <tjwilkinsonn@gmail.com>
2018-10-04 16:25:33 -0700

    Updated Unity files
    Instead of starting with a new Unity project, copied in the files from the Unity project "Times Past PT1" (PT1 = Prototype 1)

---
+++ Times Past PT1/Assets/CameraMove.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour {
+
+    GameObject playerObj;
+    Vector3 cameraOffset;
+
+	// Use this for initialization
+	void Start () {
+        playerObj = GameObject.Find("Harriet");
+
+        cameraOffset = new Vector3(0, 1, -3);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        transform.position = playerObj.transform.position + cameraOffset;
+		
+	}
+}
---
+++ Times Past PT1/Assets/SwitchCharacter.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchCharacter : MonoBehaviour {
+
+    GameObject harriet, basil;
+    int characterSelect;
+
+    // Use this for initialization
+    void Start () {
+        characterSelect = 1;
+
+        harriet = GameObject.Find("Harriet");
+        basil = GameObject.Find("Basil");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        //change character when pressing space bar
+        if (Input.GetKeyDown("space"))
+        {
+            //Debug.Log("Space bar pressed");
+
+            //update characterSelect value
+            if (characterSelect == 1) {
+
+                characterSelect = 2;
+
+            } else if (characterSelect == 2) {
+
+                characterSelect = 1;
+            }
+        }
+
+        if (characterSelect == 1) {
+            //Enable Harriet's Character Controller
+            if (harriet.GetComponent<CharacterController>())
+            {
+                //harriet.GetComponent<CharacterController>().enabled = true;
+                harriet.GetComponent<MovementClass>().enabled = true;
+            }
+            //Disable Basil's Character Controller
+            if (basil.GetComponent<CharacterController>())
+            {
+                //basil.GetComponent<CharacterController>().enabled = false;
+                basil.GetComponent<MovementClass>().enabled = false;
+            }
+        }
+        else if (characterSelect == 2) {
+            //Disable Harriet's Character Controller
+            if (harriet.GetComponent<CharacterController>())
+            {
+                //harriet.GetComponent<CharacterController>().enabled = false;
+                harriet.GetComponent<MovementClass>().enabled = false;
+            }
+            //Enable Basil's Character Controller
+            if (basil.GetComponent<CharacterController>())
+            {
+                //basil.GetComponent<CharacterController>().enabled = true;
+                basil.GetComponent<MovementClass>().enabled = true;
+            }
+
+            /*
+             * player.SetActive(t/f); doesn't work because toggles visability
+             * try this with toggling environment items
+            harriet.SetActive(false);
+            basil.SetActive(true);
+            */
+        }
+    }
+}
