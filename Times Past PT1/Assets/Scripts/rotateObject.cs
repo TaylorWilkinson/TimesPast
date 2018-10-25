f14b303bc9e1637eefae3dc48804a96b7dfeaedb
Taylor Wilkinson <tjwilkinsonn@gmail.com>
2018-10-25 09:35:15 -0700

    Updated rotateObject script to have public int to edit within Unity

--- Times Past PT1/Assets/Scripts/rotateObject.cs
+++ Times Past PT1/Assets/Scripts/rotateObject.cs
@@ -6,19 +6,25 @@ public class rotateObject : MonoBehaviour {
 
     int face = 0;
 
+    public int degreesOfRotation;
+    int faceValue;
+
 	// Use this for initialization
 	void Start () {
-		
+        faceValue = ((360 / degreesOfRotation) - 1);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        if (face > 3) face = 0;
-	}
+        Debug.Log("FACE VALUE = " + faceValue);
+        //if (face > 3) face = 0;
+        if (face > faceValue) face = 0;
+    }
 
     private void OnMouseDown()
     {
-        transform.Rotate(new Vector3(0, 90, 0));
+        //transform.Rotate(new Vector3(0, 90, 0));
+        transform.Rotate(new Vector3(0, degreesOfRotation, 0));
         face++;
     }
 
