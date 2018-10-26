02e98c0153ca8d85f1655672984cac913980e884
Unknown <jonathan128_n@hotmail.com>
2018-10-26 09:44:49 -0700

    Spot lights with shafts
    Still trying line render but bloom is not working despite TA help. Light shafts need to be adjusted based on preference. Rotation working.

--- Times Past PT1/Assets/Scripts/rotateObject.cs
+++ Times Past PT1/Assets/Scripts/rotateObject.cs
@@ -4,22 +4,42 @@ using UnityEngine;
 
 public class rotateObject : MonoBehaviour {
 
-    int face = 0;
+    int face;
+    GameObject left, middle, right;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    // Use this for initialization
+    void Start () {
+
+        left = GameObject.Find("Point Light Left");
+        middle = GameObject.Find("Point Light Middle");
+        right = GameObject.Find("Point Light Right");
+
+        face = 0;
+    }
 	
 	// Update is called once per frame
 	void Update () {
-        if (face > 3) face = 0;
-	}
+
+        if (face > 7) face = 0;
+    }
 
     private void OnMouseDown()
     {
-        transform.Rotate(new Vector3(0, 90, 0));
+        transform.Rotate(new Vector3(0, 45, 0));
         face++;
+
+        if (this.name == "MirrorLeft")
+        {
+            left.transform.Rotate(new Vector3(0, -45, 0));
+        }
+        else if (this.name == "MirrorMiddle")
+        {
+            middle.transform.Rotate(new Vector3(0, -45, 0));
+        }
+        else if (this.name == "MirrorRight")
+        {
+            right.transform.Rotate(new Vector3(0, -45, 0));
+        }
     }
 
     public int getFace()
--- Times Past PT1/Assets/Scripts/showLight.cs
+++ Times Past PT1/Assets/Scripts/showLight.cs
@@ -4,99 +4,78 @@ using UnityEngine;
 
 public class showLight : MonoBehaviour {
 
-    GameObject right1, right2, right3, right4, left1, left2, left3, left4, leftMirror, rightMirror;
-    rotateObject roRight, roLeft;
+    GameObject left, middle, right, leftMirror, middleMirror, rightMirror;
+    rotateObject roLeft, roMiddle, roRight;
+    bool lit;
 
     // Use this for initialization
     void Start () {
 
-        right1 = GameObject.Find("Cylinder right1");
-        right2 = GameObject.Find("Cylinder right2");
-        right3 = GameObject.Find("Cylinder right3");
-        right4 = GameObject.Find("Cylinder right4");
-
-        left1 = GameObject.Find("Cylinder left1");
-        left2 = GameObject.Find("Cylinder left2");
-        left3 = GameObject.Find("Cylinder left3");
-        left4 = GameObject.Find("Cylinder left4");
+        left = GameObject.Find("Point Light Left");
+        middle = GameObject.Find("Point Light Middle");
+        right = GameObject.Find("Point Light Right");
 
         leftMirror = GameObject.Find("MirrorLeft");
+        middleMirror = GameObject.Find("MirrorMiddle");
         rightMirror = GameObject.Find("MirrorRight");
 
-        roRight = rightMirror.GetComponent<rotateObject>();
         roLeft = leftMirror.GetComponent<rotateObject>();
+        roMiddle = middleMirror.GetComponent<rotateObject>();
+        roRight = rightMirror.GetComponent<rotateObject>();
 
-        //right3.SetActive(true);
-
+        lit = false;
     }
 	
 	// Update is called once per frame
 	void Update () {
-        if (roRight.getFace() == 0)
-        {
-            right1.SetActive(false);
-            right2.SetActive(false);
-            right3.SetActive(true);
-            right4.SetActive(false);
 
-        } else if(roRight.getFace() == 1)
+        if (roLeft.getFace() == 0 || roLeft.getFace() == 7 || roLeft.getFace() == 6)
         {
-            right1.SetActive(false);
-            right2.SetActive(false);
-            right3.SetActive(false);
-            right4.SetActive(true);
-
-        } else if (roRight.getFace() == 2)
+            left.SetActive(true);
+         
+        } else if (roLeft.getFace() == 3 || roLeft.getFace() == 4 || roLeft.getFace() == 5 || 
+            roLeft.getFace() == 2 || roLeft.getFace() == 1)
         {
-            right1.SetActive(true);
-            right2.SetActive(false);
-            right3.SetActive(false);
-            right4.SetActive(false);
-
-        } else if (roRight.getFace() == 3)
-        {
-            right1.SetActive(false);
-            right2.SetActive(true);
-            right3.SetActive(false);
-            right4.SetActive(false);
-
+            left.SetActive(false);
         }
 
-        if (roLeft.getFace() == 0)
+        if (roMiddle.getFace() == 5 || roMiddle.getFace() == 6 || roMiddle.getFace() == 7)
         {
-            left1.SetActive(false);
-            left2.SetActive(false);
-            left3.SetActive(true);
-            left4.SetActive(false);
-
-        }
-        else if (roLeft.getFace() == 1)
+            if (lit == true)
+            {
+                middle.SetActive(true);
+            }
+            else if (lit == false)
+            {
+                middle.SetActive(false);
+            }
+           
+        } else if (roMiddle.getFace() == 0 || roMiddle.getFace() == 1 || roMiddle.getFace() == 2
+            || roMiddle.getFace() == 3 || roMiddle.getFace() == 4)
         {
-            left1.SetActive(false);
-            left2.SetActive(false);
-            left3.SetActive(false);
-            left4.SetActive(true);
-
-        }
-        else if (roLeft.getFace() == 2)
-        {
-            left1.SetActive(true);
-            left2.SetActive(false);
-            left3.SetActive(false);
-            left4.SetActive(false);
-
-        }
-        else if (roLeft.getFace() == 3)
-        {
-            left1.SetActive(false);
-            left2.SetActive(true);
-            left3.SetActive(false);
-            left4.SetActive(false);
-
+            middle.SetActive(false);
         }
 
+        if (roRight.getFace() == 5 || roRight.getFace() == 6 || roRight.getFace() == 7)
+        {
+            right.SetActive(true);
+
+            if (roRight.getFace() == 6)
+            {
+                lit = true;
+            } 
+            else
+            {
+                lit = false;
+            }
+            
+        } else if (roRight.getFace() == 0 || roRight.getFace() == 1 || roRight.getFace() == 2
+            || roRight.getFace() == 3 || roRight.getFace() == 4)
+        {
+            right.SetActive(false);
+            lit = false;
+        }
 
     }
-
     
 }
