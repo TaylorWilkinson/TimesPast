4ccc9fec39150dac344b93997deef58d4e4e87ab
Unknown <jonathan128_n@hotmail.com>
2018-10-26 14:05:08 -0700

    Line Renderer Lights with extra mirror
    Mirrors now reflect line lights. Decided to add extra mirror and now looking for bugs.

---
+++ Times Past PT1/Assets/Scripts/RaycastReflection.cs
@@ -0,0 +1,88 @@
+﻿
+// Script taken from https://gamedev.stackexchange.com/questions/142435/stopping-a-reflecting-raycast-linerender-laser
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent (typeof(LineRenderer))]
+public class RaycastReflection : MonoBehaviour
+{
+    //this game object's Transform
+    private Transform goTransform;
+    //the attached line renderer
+    private LineRenderer lineRenderer;
+    //a ray
+    private Ray ray;
+    //a RaycastHit variable, to gather informartion about the ray's collision
+    private RaycastHit hit;
+    //reflection direction
+    private Vector3 direction;
+    //the number of reflections
+    public int nReflections = 2;
+    //max length
+    public float maxLength = 100f;
+    //the number of points at the line renderer
+    private int numPoints;
+    //private int pointCount;
+
+    Vector3 temp;
+    Vector3 idk;
+    int y;
+
+    void Awake()
+    {
+        //get the attached Transform component  
+        goTransform = this.GetComponent<Transform>();
+        //get the attached LineRenderer component  
+        lineRenderer = this.GetComponent<LineRenderer>();
+
+        temp = goTransform.position;
+        if (this.name == "MirrorMiddle" || this.name == "MirrorRight") { 
+        temp.y += 2.75f;
+            if (this.name == "MirrorRight")
+            {
+                y = 215;
+            } else if (this.name == "MirrorMiddle") 
+            {
+                y = 180;
+            }
+         } else if(this.name == "MirrorLeft")
+        {
+            temp.y += 0.25f;
+            y = -120;
+        }
+
+       
+    }
+    void Update ()
+    {
+        idk = Quaternion.Euler(0, y, 0) * transform.forward;
+
+        //clamp the number of reflections between 1 and int capacity  
+        nReflections = Mathf.Clamp (nReflections, 1, nReflections);
+        ray = new Ray (/*goTransform.position*/temp, /*goTransform.forward*/ idk);    
+        //start with just the origin
+        lineRenderer.positionCount = 1;
+        lineRenderer.SetPosition (0, /*goTransform.position*/ temp);
+        float remainingLength = maxLength;
+        //bounce up to n times
+        for (int i = 0; i < nReflections; i++) {
+            // ray cast
+            if (Physics.Raycast (ray.origin, ray.direction, out hit, remainingLength)) {
+                //we hit, update line renderer
+                lineRenderer.positionCount += 1;
+                lineRenderer.SetPosition (lineRenderer.positionCount - 1, hit.point);
+                // update remaining length and set up ray for next loop
+                remainingLength -= Vector3.Distance (ray.origin, hit.point);
+                ray = new Ray (hit.point, Vector3.Reflect(ray.direction, hit.normal));
+                // break loop if we don't hit a Mirror
+                if (hit.collider.tag != "Mirror")
+                    break;
+            } else {
+                // We didn't hit anything, draw line to end of ramainingLength
+                lineRenderer.positionCount += 1;
+                lineRenderer.SetPosition (lineRenderer.positionCount - 1, ray.origin + ray.direction * remainingLength);
+                break;
+            }
+        }
+    }
+}
--- Times Past PT1/Assets/Scripts/showLight.cs
+++ Times Past PT1/Assets/Scripts/showLight.cs
@@ -7,6 +7,7 @@ public class showLight : MonoBehaviour {
     GameObject left, middle, right, leftMirror, middleMirror, rightMirror;
     rotateObject roLeft, roMiddle, roRight;
     bool lit;
+    LineRenderer lrLeft, lrMiddle, lrRight;
 
     // Use this for initialization
     void Start () {
@@ -23,6 +24,10 @@ public class showLight : MonoBehaviour {
         roMiddle = middleMirror.GetComponent<rotateObject>();
         roRight = rightMirror.GetComponent<rotateObject>();
 
+        lrLeft = roLeft.GetComponent<LineRenderer>();
+        lrMiddle = roMiddle.GetComponent<LineRenderer>();
+        lrRight = roRight.GetComponent<LineRenderer>();
+
         lit = false;
     }
 	
@@ -31,36 +36,46 @@ public class showLight : MonoBehaviour {
 
         if (roLeft.getFace() == 0 || roLeft.getFace() == 7 || roLeft.getFace() == 6)
         {
-            left.SetActive(true);
+            //left.SetActive(true);
+            lrLeft.enabled = true;
          
         } else if (roLeft.getFace() == 3 || roLeft.getFace() == 4 || roLeft.getFace() == 5 || 
             roLeft.getFace() == 2 || roLeft.getFace() == 1)
         {
-            left.SetActive(false);
+            //left.SetActive(false);
+            lrLeft.enabled = false;
         }
 
-        if (roMiddle.getFace() == 5 || roMiddle.getFace() == 6 || roMiddle.getFace() == 7)
+        if (roMiddle.getFace() == 1 || roMiddle.getFace() == 2 || roMiddle.getFace() == 3)
         {
             if (lit == true)
             {
-                middle.SetActive(true);
+                //middle.SetActive(true);
+                lrMiddle.enabled = true;
             }
             else if (lit == false)
             {
-                middle.SetActive(false);
+                //middle.SetActive(false);
+                lrMiddle.enabled = false;
             }
            
-        } else if (roMiddle.getFace() == 0 || roMiddle.getFace() == 1 || roMiddle.getFace() == 2
-            || roMiddle.getFace() == 3 || roMiddle.getFace() == 4)
+        } else if (roMiddle.getFace() == 5 || roMiddle.getFace() == 6 || roMiddle.getFace() == 7
+            || roMiddle.getFace() == 0 || roMiddle.getFace() == 4)
         {
-            middle.SetActive(false);
+
+            if(roMiddle.getFace() == 1 || roMiddle.getFace() == 2 || roMiddle.getFace() == 3){
+                lrMiddle.enabled = true;
+            }
+            //middle.SetActive(false);
+            lrMiddle.enabled = false;
         }
 
         if (roRight.getFace() == 5 || roRight.getFace() == 6 || roRight.getFace() == 7)
         {
-            right.SetActive(true);
+            //right.SetActive(true);
+            lrRight.enabled = true;
 
-            if (roRight.getFace() == 6)
+            if (roRight.getFace() == 5)
             {
                 lit = true;
             } 
@@ -72,7 +87,8 @@ public class showLight : MonoBehaviour {
         } else if (roRight.getFace() == 0 || roRight.getFace() == 1 || roRight.getFace() == 2
             || roRight.getFace() == 3 || roRight.getFace() == 4)
         {
-            right.SetActive(false);
+            //right.SetActive(false);
+            lrRight.enabled = false;
             lit = false;
         }
 
